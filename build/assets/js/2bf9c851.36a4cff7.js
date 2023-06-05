aa7d74312140c81e74fc9656b4cad03ed27bbf16
xactlyblue <xactlybluestudios@gmail.com>
2023-06-05 19:04:27 -0400

    Update documentation build

--- build/assets/js/2bf9c851.36a4cff7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[225],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/xactlyblue/sketch/blob/main","baseUrl":"/sketch/","classOrder":["Sketch","Sketchbook","Drawing","Renderer"],"apiCategories":[]}')}}]);
